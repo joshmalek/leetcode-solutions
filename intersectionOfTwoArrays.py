4d8bbdd4d79683030c1864b73cb5391a08d6dd28
17jmalek98 <17jmalek98@gmail.com>
2020-05-15 12:02:52 -0700

    added notes

---
+++ intersectionOfTwoArrays.py
@@ -0,0 +1,9 @@
+# Given two arrays, write a function to compute their intersection.
+
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [4,9]
+
+
+
+from typing import List
+def intersect(nums1: List[int], nums2: List[int]) -> List[int]:
