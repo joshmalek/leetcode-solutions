8bc25aa75320b80073dda19dc5460a9be251e1a9
17jmalek98 <17jmalek98@gmail.com>
2020-05-24 16:27:55 -0700

    finished solution

--- Strings/stringToInt.py
+++ Strings/stringToInt.py
@@ -16,3 +16,33 @@
 # lstrip() all the white space from the left side, which leaves the number. Then look for a plus or minus if it exists.
 # After that, iterate through the string and split out where the number stops.  Then, convert this number using int.  If
 # this number is larger than INT MAX, return INT MAX (2^31).  This is O(n) runtime.
+
+def myAtoi(str: str) -> int:
+    s = str.strip()
+    if(len(s) == 0):
+        return 0
+    s_index = 0
+    sign = 1
+
+    if(s[0] == '-'):
+        sign = -1
+        s_index = 1
+    elif(s[0] == '+'):
+        sign = 1
+        s_index = 1
+    elif(s[0].isdigit() == False):
+        return 0
+    i = s_index
+    while(i < len(s) and s[i].isdigit()):
+        i+=1
+    if(s_index == i):
+        return 0
+    num = sign * int(s[s_index:i])
+    if(num < pow(-2,31)):
+        return(pow(-2,31))
+    elif(num > pow(2,31)-1):
+        return(pow(2,31)-1)
+    else:
+        return num
+
+print(myAtoi("+"))
