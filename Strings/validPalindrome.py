5da4583c3ce3cc207aaf639be85d38bf3ea027af
17jmalek98 <17jmalek98@gmail.com>
2020-05-24 15:57:17 -0700

    finished solution

--- Strings/validPalindrome.py
+++ Strings/validPalindrome.py
@@ -5,7 +5,24 @@
 # Notes:
 # A palindrome is a word or statment that reads the same backwards as forward.  So, my idea is to first strip the
 # whitespace, then split into two based on length.  Then, reverse the second half and compare it to the first.  if
-# they are equal, then return true.  I think this would be O(n).
+# they are equal, then return true.  I think this would be O(n), with O(n) space.
 
-def isPalindrome(self, s: str) -> bool:
-    
+# Another way of doing this is a 2 pointer style, one pointing to the first and the other to the last.  Then,
+# iterate through the string and compare that they are equal.  This is also O(n), but with O(1) space.
+
+def isPalindrome(s: str) -> bool:
+    e = []
+    for i in s:
+        if(i.isalnum()):
+            e.append(i.lower())
+    s = "".join(e)
+    spl = len(s)//2
+    if(len(s)%2):
+        a = s[:spl]
+        b = s[spl+1:]
+    else:
+        a = s[:spl]
+        b = s[spl:]
+    return a == b[::-1]
+
+print(isPalindrome("race a car"))
