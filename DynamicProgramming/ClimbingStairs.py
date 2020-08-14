bf144a0b7c195fbc58a93330aa2d50d74ec5a78d
Josh Malek <36347275+joshmalek@users.noreply.github.com>
2020-08-13 17:31:51 -0700

    Create ClimbingStairs.py

---
+++ DynamicProgramming/ClimbingStairs.py
@@ -0,0 +1,17 @@
+# Link: https://leetcode.com/problems/climbing-stairs/
+
+# Notes: A basic backtracking algorithm.  We try and find the answers for all the subproblems before the main problem, and recursively work our way down.
+# We cache our subproblems to save compute time.
+
+def helper(self,i,n,cache)-> int:
+    if(i > n):
+        return 0
+    if(i == n):
+        return 1
+    if(i in cache):
+        return cache[i]
+    cache[i] = self.helper(i+1,n,cache)+self.helper(i+2,n,cache)
+    return cache[i]
+
+cache = {}
+return(self.helper(0,n,cache))
