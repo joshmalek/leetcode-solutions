83874a6b97480e769bbacaa627f528a304882361
Josh Malek <17jmalek98@gmail.com>
2020-12-22 13:07:28 -0800

    added new dynamic programming solution

--- DynamicProgramming/ClimbingStairs.py
+++ DynamicProgramming/ClimbingStairs.py
@@ -1,5 +1,21 @@
 # Link: https://leetcode.com/problems/climbing-stairs/
 
+# Notes: Actual dynamic programming here.  Very simple and efficient. Stores the results of the runs before it.
+def climbStairs(self, n: int) -> int:
+    if(n == 0):
+        return 0
+    elif(n == 1):
+        return 1
+    elif(n == 2):
+        return 2
+    else:
+        dp = [0,1,2]
+        for i in range(3, n+1):
+            dp.append(dp[i-1] + dp[i-2])
+        return dp[n]
+
+
+
 # Notes: A basic backtracking algorithm.  We try and find the answers for all the subproblems before the main problem, and recursively work our way down.
 # We cache our subproblems to save compute time through the recursion.
 
