8aeb04b58a98195f7d92817c355541ae968194cb
Josh Malek <17jmalek98@gmail.com>
2020-12-26 20:07:17 -0800

    add buy and sell stock DP

---
+++ DynamicProgramming/BuyAndSellStock.py
@@ -0,0 +1,6 @@
+def maxProfit(self, prices: List[int]) -> int:
+    max_profit = 0
+    for k in range(len(prices)-1,0,-1):
+        if(min(prices[0:k]) < prices[k]):
+            max_profit = max(max_profit,prices[k] - min(prices[0:k]))
+    return max_profit
