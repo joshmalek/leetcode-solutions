bb31dbf1f7f2599d585703b692b1c25eccf4f000
Josh Malek <17jmalek98@gmail.com>
2020-12-28 15:59:10 -0800

    add link

--- Arrays/ToeplitzMatrix.py
+++ Arrays/ToeplitzMatrix.py
@@ -1,3 +1,6 @@
+#Link: https://leetcode.com/problems/toeplitz-matrix/submissions/
+
+
 def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
     arr1 = [[1,2,3,4],[5,1,2,3],[6,5,1,2]]
     arr2 = [[1,2,3,4],[5,1,9,3],[6,5,1,2]]
@@ -6,13 +9,13 @@ def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
     C = len(matrix[0])
     #for each element in the first row of the array
     for col in range(0,C):
-        start = matrix[0][c]
+        start = matrix[0][col]
         for k in range(0,min(R, C-col)):
             print(matrix[k][col+k])
             if matrix[k][col+k] != start:
                 return False
     for row in range(0,R):
-        start = matrix[R][0]
+        start = matrix[row][0]
         for k in range(0,min(R-row, C)):
             print(matrix[row+k][k])
             if matrix[row+k][k] != start:
