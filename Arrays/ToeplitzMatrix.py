3499d4bd17f238707d83348145cc4e88156fba35
Josh Malek <17jmalek98@gmail.com>
2020-12-28 15:58:04 -0800

    add toeplitz matrix

--- Arrays/ToeplitzMatrix.py
+++ Arrays/ToeplitzMatrix.py
@@ -3,18 +3,18 @@ def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
     arr2 = [[1,2,3,4],[5,1,9,3],[6,5,1,2]]
 
     R = len(matrix)
-    C = len(arr1[0])
+    C = len(matrix[0])
     #for each element in the first row of the array
     for col in range(0,C):
         start = matrix[0][c]
         for k in range(0,min(R, C-col)):
             print(matrix[k][col+k])
             if matrix[k][col+k] != start:
-            return False
+                return False
     for row in range(0,R):
-        start = arr1[R][0]
+        start = matrix[R][0]
         for k in range(0,min(R-row, C)):
-            print(arr1[row+k][k])
-            if arr1[row+k][k] != start:
-            return False
+            print(matrix[row+k][k])
+            if matrix[row+k][k] != start:
+                return False
     return True
