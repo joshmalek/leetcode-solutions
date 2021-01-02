7483167a563195956c3b963f6ad6debe1680b95f
Josh Malek <17jmalek98@gmail.com>
2021-01-01 22:22:55 -0800

    add word count engine

---
+++ Strings/WordCountEngine.py
@@ -0,0 +1,22 @@
+
+import string 
+from collections import OrderedDict
+def word_count_engine(document):
+  no_punc = ""
+  for c in document:
+    if(c not in string.punctuation):
+      no_punc += c
+  word_split = no_punc.split()
+  for i in range(0,len(word_split)):
+    word_split[i] = word_split[i].lower()
+  count = OrderedDict()
+  for word in word_split:
+    count[word] = word_split.count(word)
+  count = OrderedDict(sorted(count.items(), key=lambda kv: kv[1], reverse=True))
+  ret = []
+  for key in count:
+    ret.append([key,str(count[key])])
+  print(ret)
+  return ret
+document = "Practice makes perfect. you'll only get Perfect by practice. just practice!"
+word_count_engine(document)
